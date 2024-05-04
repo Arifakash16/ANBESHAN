5d42d4e6bfa8a8050cc326a85c1c553bad3c989b
Arifakash16 <mdarifulislam6898@gmail.com>
2024-05-04 11:30:25 +0600

    all link could  access successfully

--- src/Components/SearchScreen/searchData/Data.js
+++ src/Components/SearchScreen/searchData/Data.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom';
+
 
 const Data = ({dataParams}) => {
     return (
+
         <div className=''>
-          <Link to={dataParams.displayLink}>{dataParams.displayLink}</Link>   {"  "} <i className='fa fa-angle-down'></i>
-         <Link to={dataParams.formattedUrl}> <h3>{dataParams.title}</h3></Link>
-<p>{dataParams.snippet}</p>
+            <a href={dataParams.formattedUrl}>{dataParams.displayLink}</a>   {"  "} <i className='fa fa-angle-down'></i>
+           
+            <a href={dataParams.formattedUrl}>
+                <h3>{dataParams.title}</h3>
+            </a>
+            <p>{dataParams.snippet}</p>
         </div>
+
+
+
+      
     );
 };
 
--- src/Components/SearchScreen/searchData/index.js
+++ src/Components/SearchScreen/searchData/index.js
@@ -7,10 +7,10 @@ const SearchData = ({googleData}) => {
     console.log('Google Data', googleData);
     return (
         <div className=' p-5 ml-1'>
-          <p>All {googleData?.searchInformation.
-formattedTotalResults} found in {googleData?.searchInformation.formattedSearchTime} sec </p>
+          {/* <p>All {googleData?.searchInformation.
+formattedTotalResults} found in {googleData?.searchInformation.formattedSearchTime} sec </p> */}
           {
-                 googleData?.items.map(data=><Data dataParams={data} key={data.id}></Data>)
+                 googleData?.items?.map(data=><Data dataParams={data} key={data.id}></Data>)
           }
     
         </div>
